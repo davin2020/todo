f6353d0a17f13bd7f570136ef990902ec63f2701
Davin <davin.hayden@outlook.com>
2020-11-26 22:15:44 +0000

    added routes for /,  /markAsComplete/{id}, /completedTasks, /saveTask, and /deleteTask{id} as they have Controller & Factory classes working ok

--- app/routes.php
+++ app/routes.php
@@ -7,9 +7,20 @@ use Slim\Interfaces\RouteCollectorProxyInterface as Group;
 return function (App $app) {
     $container = $app->getContainer();
 
-    $app->get('/', function ($request, $response, $args) use ($container) {
-        $renderer = $container->get('renderer');
-        return $renderer->render($response, "index.php", $args);
-    });
+    //original route
+//    $app->get('/', function ($request, $response, $args) use ($container) {
+//        $renderer = $container->get('renderer');
+//        return $renderer->render($response, "index.php", $args);
+//    });
+
+    // add in custom routes here
+    $app->get('/', 'HomepageController');
+    $app->get('/markAsComplete/{id}', 'MarkCompleteController');
+    $app->get('/completedTasks', 'CompletedTasksPageController');
+    $app->post('/saveTask', 'SaveTaskController');
+
+    //need to create controller & factory for this route
+    $app->get('/deleteTask/{id}', 'DeleteTaskController');
+
 
 };
