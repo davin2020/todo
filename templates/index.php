eef86c2163358552e58d5e00f08b3bd24cab1dcd
Davin <davin.hayden@outlook.com>
2020-11-26 22:41:37 +0000

    adjusted link to Google Font to be 400 weight

--- templates/index.php
+++ templates/index.php
@@ -4,31 +4,8 @@
 <head>
     <meta charset="utf-8"/>
     <title>To Do List</title>
-    <link href='//fonts.googleapis.com/css?family=Lato:300' rel='stylesheet' type='text/css'>
-    <style>
-        body {
-            margin: 50px 0 0 0;
-            padding: 0;
-            width: 100%;
-            font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-            text-align: center;
-            color: #000;
-            font-size: 18px;
-            background-color: #FCFFCD;
-        }
-
-        h1 {
-            color: #FF8F35;
-            letter-spacing: -3px;
-            font-family: 'Lato', sans-serif;
-            font-size: 100px;
-            font-weight: 200;
-            margin-bottom: 0;
-        }
-        h2 {
-            color: #FF8F35;
-        }
-    </style>
+    <link href="style.css" type="text/css" rel="stylesheet">
+    <link href='//fonts.googleapis.com/css?family=Lato:400' rel='stylesheet' type='text/css'>
 </head>
 
 <body>
